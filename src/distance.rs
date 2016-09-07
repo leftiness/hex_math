1b3b70c5907e49d512ddba21aace41a04d84b326
Brandon Parmenter <leftiness@gmail.com>
2016-09-06 20:20:43 -0400

    Closes #8: Can now add &Point + &Point

--- src/distance.rs
+++ src/distance.rs
@@ -26,13 +26,12 @@ use point::Point;
 /// assert_eq!(9, distance(&point, &other));
 /// ```
 pub fn distance(point: &Point, other: &Point) -> i32 {
-  let point = point.clone();
-  let other = other.clone();
   let diff: Point = point - other;
   let base = distance_2d(&point, &other);
   let height = diff.t.abs();
+  let distance = base + height;
 
-  base + height
+  distance
 }
 
 /// Calculate the manhattan distance between two points ignoring height
@@ -50,8 +49,6 @@ pub fn distance(point: &Point, other: &Point) -> i32 {
 /// assert_eq!(4, distance_2d(&point, &other));
 /// ```
 pub fn distance_2d(point: &Point, other: &Point) -> i32 {
-  let point = point.clone();
-  let other = other.clone();
   let diff: Point = point - other;
   let distance = (diff.q.abs() + diff.r.abs() + diff.s.abs()) / 2;
 
