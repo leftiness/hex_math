0ed013f30555d6d3b2ca1bab065eac7cd1538e51
Brandon Parmenter <leftiness@gmail.com>
2016-10-29 12:03:30 -0400

    Issue #60: Added function for determining direction between two points

--- src/enums/direction.rs
+++ src/enums/direction.rs
@@ -1,3 +1,8 @@
+use std::convert::From;
+
+use structs::Point;
+use traits::HasValues;
+
 /// Enum describing positions in relation to a point
 #[derive(Debug, PartialEq)]
 pub enum Direction {
@@ -58,6 +63,64 @@ impl Direction {
       &Direction::Northeast => Direction::Southwest,
       &Direction::Up        => Direction::Down,
       &Direction::Down      => Direction::Up,
+    }
+
+  }
+
+}
+
+impl <'a, 'b, T> From<(&'a T, &'b T)> for Direction where T: HasValues {
+
+  /// Get the direction from one point to another
+  ///
+  /// # Example
+  ///
+  /// ```
+  /// use hex_math::{Direction, Point};
+  ///
+  /// let point:     Point = Point::new(1, 2, 5);
+  /// let east:      Point = Point::new(2, 2, 5);
+  /// let southeast: Point = Point::new(1, 3, 5);
+  /// let southwest: Point = Point::new(0, 3, 5);
+  /// let west:      Point = Point::new(0, 2, 5);
+  /// let northwest: Point = Point::new(1, 1, 5);
+  /// let northeast: Point = Point::new(2, 1, 5);
+  /// let up:        Point = Point::new(1, 2, 6);
+  /// let down:      Point = Point::new(1, 2, 4);
+  ///
+  /// let from = Direction::from;
+  ///
+  /// assert_eq!(Direction::East,      from((&point, &east)));
+  /// assert_eq!(Direction::Southeast, from((&point, &southeast)));
+  /// assert_eq!(Direction::Southwest, from((&point, &southwest)));
+  /// assert_eq!(Direction::West,      from((&point, &west)));
+  /// assert_eq!(Direction::Northwest, from((&point, &northwest)));
+  /// assert_eq!(Direction::Northeast, from((&point, &northeast)));
+  /// assert_eq!(Direction::Up,        from((&point, &up)));
+  /// assert_eq!(Direction::Down,      from((&point, &down)));
+  /// ```
+  fn from((p0, p1): (&'a T, &'b T)) -> Direction {
+
+    let p0: Point = p0.values().into();
+    let p1: Point = p1.values().into();
+    let diff: Point = &p1 - &p0;
+
+    let (dq, dr, dt) = diff.values();
+
+    match dt.signum() {
+       1 => return Direction::Up,
+      -1 => return Direction::Down,
+       _ => (),
+    }
+
+    return match (dq.signum(), dr.signum()) {
+      ( 1,  0) => Direction::East,
+      ( 0,  1) => Direction::Southeast,
+      (-1,  1) => Direction::Southwest,
+      (-1,  0) => Direction::West,
+      ( 0, -1) => Direction::Northwest,
+      ( 1, -1) => Direction::Northeast,
+      _ => panic!(),
     }
 
   }
