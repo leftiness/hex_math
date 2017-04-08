46bac73e11adbe3a46cd247e183a0284a3657c85
Brandon Parmenter <leftiness@gmail.com>
2017-04-08 16:35:03 -0400

    Issue #33: Refactor ring

--- src/ring.rs
+++ src/ring.rs
@@ -6,47 +6,6 @@ use traits::HasValues;
 use travel::travel;
 
 /// Find points in a spherical ring of a provided radius
-///
-/// # Example
-///
-/// ```
-/// use hex_math::{Point, ring};
-/// use std::collections::HashSet;
-///
-/// let point: Point = Point::new(1, 2, 5);
-/// let set: HashSet<Point> = ring(&point, 2);
-///
-/// assert!(set.contains(&Point::new(1, 0, 5)));
-/// assert!(set.contains(&Point::new(2, 0, 5)));
-/// assert!(set.contains(&Point::new(3, 0, 5)));
-/// assert!(set.contains(&Point::new(3, 1, 5)));
-/// assert!(set.contains(&Point::new(3, 2, 5)));
-/// assert!(set.contains(&Point::new(2, 3, 5)));
-/// assert!(set.contains(&Point::new(1, 4, 5)));
-/// assert!(set.contains(&Point::new(0, 4, 5)));
-/// assert!(set.contains(&Point::new(-1, 4, 5)));
-/// assert!(set.contains(&Point::new(-1, 3, 5)));
-/// assert!(set.contains(&Point::new(-1, 2, 5)));
-/// assert!(set.contains(&Point::new(0, 1, 5)));
-///
-/// assert!(set.contains(&Point::new(1, 1, 6)));
-/// assert!(set.contains(&Point::new(2, 1, 6)));
-/// assert!(set.contains(&Point::new(2, 2, 6)));
-/// assert!(set.contains(&Point::new(1, 3, 6)));
-/// assert!(set.contains(&Point::new(0, 3, 6)));
-/// assert!(set.contains(&Point::new(0, 2, 6)));
-///
-/// assert!(set.contains(&Point::new(1, 1, 4)));
-/// assert!(set.contains(&Point::new(2, 1, 4)));
-/// assert!(set.contains(&Point::new(2, 2, 4)));
-/// assert!(set.contains(&Point::new(1, 3, 4)));
-/// assert!(set.contains(&Point::new(0, 3, 4)));
-/// assert!(set.contains(&Point::new(0, 2, 4)));
-///
-/// assert!(set.contains(&Point::new(1, 2, 7)));
-/// assert!(set.contains(&Point::new(1, 2, 3)));
-/// assert_eq!(set.len(), 26);
-/// ```
 pub fn ring<T: HasValues>(point: &T, range: i32) -> HashSet<Point> {
   let mut set: HashSet<Point> = ring_2d(point, range);
 
@@ -68,24 +27,6 @@ pub fn ring<T: HasValues>(point: &T, range: i32) -> HashSet<Point> {
 }
 
 /// Find points at the same height in a ring of a provided radius
-///
-/// # Example
-///
-/// ```
-/// use hex_math::{Point, ring_2d};
-/// use std::collections::HashSet;
-///
-/// let point: Point = Point::new(1, 2, 5);
-/// let set: HashSet<Point> = ring_2d(&point, 1);
-///
-/// assert!(set.contains(&Point::new(1, 1, 5)));
-/// assert!(set.contains(&Point::new(2, 1, 5)));
-/// assert!(set.contains(&Point::new(2, 2, 5)));
-/// assert!(set.contains(&Point::new(1, 3, 5)));
-/// assert!(set.contains(&Point::new(0, 3, 5)));
-/// assert!(set.contains(&Point::new(0, 2, 5)));
-/// assert_eq!(set.len(), 6);
-/// ```
 pub fn ring_2d<T: HasValues>(point: &T, range: i32) -> HashSet<Point> {
   let mut set: HashSet<Point> = HashSet::new();
   let mut point: Point = travel(point, &Direction::Northwest, range);
@@ -102,3 +43,58 @@ pub fn ring_2d<T: HasValues>(point: &T, range: i32) -> HashSet<Point> {
   set
 }
 
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn ring() {
+    let point: Point = Point::new(1, 2, 5);
+    let set: HashSet<Point> = super::ring(&point, 2);
+
+    assert!(set.contains(&Point::new(1, 0, 5)));
+    assert!(set.contains(&Point::new(2, 0, 5)));
+    assert!(set.contains(&Point::new(3, 0, 5)));
+    assert!(set.contains(&Point::new(3, 1, 5)));
+    assert!(set.contains(&Point::new(3, 2, 5)));
+    assert!(set.contains(&Point::new(2, 3, 5)));
+    assert!(set.contains(&Point::new(1, 4, 5)));
+    assert!(set.contains(&Point::new(0, 4, 5)));
+    assert!(set.contains(&Point::new(-1, 4, 5)));
+    assert!(set.contains(&Point::new(-1, 3, 5)));
+    assert!(set.contains(&Point::new(-1, 2, 5)));
+    assert!(set.contains(&Point::new(0, 1, 5)));
+
+    assert!(set.contains(&Point::new(1, 1, 6)));
+    assert!(set.contains(&Point::new(2, 1, 6)));
+    assert!(set.contains(&Point::new(2, 2, 6)));
+    assert!(set.contains(&Point::new(1, 3, 6)));
+    assert!(set.contains(&Point::new(0, 3, 6)));
+    assert!(set.contains(&Point::new(0, 2, 6)));
+
+    assert!(set.contains(&Point::new(1, 1, 4)));
+    assert!(set.contains(&Point::new(2, 1, 4)));
+    assert!(set.contains(&Point::new(2, 2, 4)));
+    assert!(set.contains(&Point::new(1, 3, 4)));
+    assert!(set.contains(&Point::new(0, 3, 4)));
+    assert!(set.contains(&Point::new(0, 2, 4)));
+
+    assert!(set.contains(&Point::new(1, 2, 7)));
+    assert!(set.contains(&Point::new(1, 2, 3)));
+    assert!(set.len() == 26);
+  }
+
+  #[test]
+  fn ring_2d() {
+    let point: Point = Point::new(1, 2, 5);
+    let set: HashSet<Point> = super::ring_2d(&point, 1);
+
+    assert!(set.contains(&Point::new(1, 1, 5)));
+    assert!(set.contains(&Point::new(2, 1, 5)));
+    assert!(set.contains(&Point::new(2, 2, 5)));
+    assert!(set.contains(&Point::new(1, 3, 5)));
+    assert!(set.contains(&Point::new(0, 3, 5)));
+    assert!(set.contains(&Point::new(0, 2, 5)));
+    assert!(set.len() == 6);
+  }
+}
